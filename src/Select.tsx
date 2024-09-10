0b3e0b19662b5706b6d44385da0148cfe9862f43
Luca Nerlich <nerlich@adobe.com>
2024-09-10 11:02:56 +0200

    refetch dependent selects only

--- src/Select.tsx
+++ src/Select.tsx
@@ -5,28 +5,28 @@ interface SelectProps {
     name: string;
     endpoint: string;
     resetOthersOnChange?: string[];
-    refreshOnChange?: string[];
 }
 
 const Select: React.FC<SelectProps> = ({name, endpoint, resetOthersOnChange = []}) => {
     const {values, options, loading, setSelectValue, resetSelect, fetchOptions} = useSelectContext();
 
     const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setSelectValue(name, event.target.value);
+        const newValue = event.target.value;
+        setSelectValue(name, newValue);
+
         if (resetOthersOnChange.length > 0) {
-            resetSelect(resetOthersOnChange);
+            resetSelect([...resetOthersOnChange]);
+            resetOthersOnChange.forEach((dependentSelect) => {
+                fetchOptions(dependentSelect, endpoint);
+            });
         }
     };
 
-    const buildEndpoint = useCallback(() => {
-        // todo pass url template via props
-        return endpoint;
-    }, [endpoint]);
+    const buildEndpoint = useCallback(() => endpoint, [endpoint]);
 
     useEffect(() => {
-        // Also re-fetch options after selection (values) change
         fetchOptions(name, buildEndpoint());
-    }, [values, name, endpoint, fetchOptions, buildEndpoint]);
+    }, [name, fetchOptions, buildEndpoint]);
 
     const isLoading = loading[name];
     const selectOptions = options[name] || [];
