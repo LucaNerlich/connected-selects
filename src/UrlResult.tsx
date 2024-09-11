64e892825eaaa83598178833019dd905bd710fbc
Luca Nerlich <nerlich@adobe.com>
2024-09-11 13:35:08 +0200

    small refactoring

--- src/UrlResult.tsx
+++ src/UrlResult.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import {useSelectContext} from "./UseSelectContext.tsx";
 import {ID} from "./ID.ts";
 
+const generateUrl = (values: Record<ID, string>): string => {
+    const useCase = values[ID.USE_CASE] ?? '';
+    const brand = values[ID.BRAND] ?? '';
+    const region = values[ID.REGION] ?? '';
+    const app = values[ID.APP] ?? '';
+    const lang = values[ID.LANG] ?? '';
+
+    return `https://endpoint.com/${useCase}/${brand}/${region}/${app}/${lang}/index.html`.toLowerCase();
+};
+
 export default function UrlResult(): React.ReactElement {
     const {values} = useSelectContext();
 
-    // /oru/audi/eu/ota/rn-2023-03-14-ftkm/de-de/tech-1-2/mode-day/index.html
     return (
         <blockquote>
-            <strong>URL</strong> {`https://endpoint.com/${values[ID.USE_CASE]}/${values[ID.BRAND]}/${values[ID.REGION]}/${values[ID.APP]}/${values[ID.LANG]}/index.html`.toLowerCase()}
+            <strong>URL</strong> {generateUrl(values)}
         </blockquote>
-    )
+    );
 }
